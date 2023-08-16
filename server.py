46c45908a36e45fa43c096d7c0cce436559540c7
asvanth <asvanthc@gmail.com>
2023-08-16 13:23:13 +0530

    assd

--- server.py
+++ server.py
@@ -1,19 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Welcome to the Calculator App!"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.json  # Assuming you're sending JSON data in the request body
+    if 'numbers' in data and isinstance(data['numbers'], list):
+        result = sum(data['numbers'])
+        return {"result": result}
+    else:
+        return {"error": "Invalid data"}
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.json
+    if 'numbers' in data and isinstance(data['numbers'], list) and len(data['numbers']) >= 2:
+        result = data['numbers'][0] - sum(data['numbers'][1:])
+        return {"result": result}
+    else:
+        return {"error": "Invalid data"}
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
